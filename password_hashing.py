cea9f7ed13dab3aadcfa15bc15c58348f8ee0690
VICTOR IJONI <51201499+CodeLuminary@users.noreply.github.com>
2021-11-06 18:06:21 +0100

    Update password_hashing.py

--- password_hashing.py
+++ password_hashing.py
@@ -1,20 +1,80 @@
+from Crypto.Hash import SHA256
+import fileinput
 
+hash = SHA256.new()
 
 should_continue = True #This determines wheather the loop should run or not
-passwordlines = [] #This holds each line of the passwd.a.txt file
+lines = [] #This holds each line of the passwd.a.txt file
+hash_passwords = []
 
+with open('passwd_a.txt') as f:
+    lines = f.readlines()
+
+count = 0
+
+for line in lines:
+    count += 1
+    hash.update(bytes(line, 'utf-8'))
+    password_dict = {
+        "Index": str(count),
+        "Usernames" : "UID" + str(count),
+        "Hashed passwords": hash.hexdigest(),
+        "plain text": line
+    }
+    
+    hash_passwords.append(password_dict)
+f.close()
+
+def getPlainPassword(hashpassword):
+    for passwds in hash_passwords:
+        if hashpassword == passwds["Hashed passwords"]:
+            return passwds["plain text"]
 
 while(should_continue):
     option = input(''' Enter 1 to print PasswordTable for the passwd.a.txt file. \n
     Enter 2 to print hash password of a particular index.\n
     Enter 3 to recover a plaintext password.\n
-    Enter c to cancel\n
+    Enter c to cancel
     \n''')
     if option == 'c':
         should_continue = False
     elif option == '1':
-        pass
+        print("Index     Usernames     Hashed passwords")
+        for passwds in hash_passwords:
+            line_str = passwds["Index"]
+
+            for i in range(0, (10 - len(passwds["Index"]))):
+                line_str += " "
+            line_str += passwds["Usernames"]
+
+            for i in range(0, (14 - len(passwds["Usernames"]))):
+                line_str += " "
+            line_str += passwds["Hashed passwords"]
+            print(line_str)
+
     elif option == '2':
-        pass
+        try:
+            option2 = int(input("Enter password index position\n"))
+
+            print("Index     Usernames     Hashed passwords")
+
+            passwds = hash_passwords[option2 - 1]
+            line_str = passwds["Index"]
+
+            for i in range(0, (10 - len(passwds["Index"]))):
+                line_str += " "
+            line_str += passwds["Usernames"]
+
+            for i in range(0, (14 - len(passwds["Usernames"]))):
+                line_str += " "
+            line_str += passwds["Hashed passwords"]
+            print(line_str)
+        except:
+            print("Please put only an integer value")
+
     elif option == '3':
         pass
+
+
+    print("\n")
+
